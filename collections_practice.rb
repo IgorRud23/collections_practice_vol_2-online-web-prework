8cf686e522de6c4d667fcb0e0432e5e007584211
Igor Rud <rud.igor23@gmail.com>
2019-01-31 21:11:16 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,5 +1,5 @@
-def begins_with_r(["ruby", "rspec", "rails"])
-["ruby", "rspec", "rails"].each do |letter|
+def begins_with_r(array)
+  array.each do |letter|
 if letter.start_with?("r")
   true
   letter += 1
