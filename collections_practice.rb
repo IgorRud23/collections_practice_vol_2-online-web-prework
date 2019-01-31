af944e863cf96deac9929796abcdd7871f532fae
Igor Rud <rud.igor23@gmail.com>
2019-01-31 21:09:41 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,8 @@
-# your code goes here
+def begins_with_r(["ruby", "rspec", "rails"])
+["ruby", "rspec", "rails"].each do |letter|
+if letter.start_with?("r")
+  return true
+else
+  return false
+end
+end
